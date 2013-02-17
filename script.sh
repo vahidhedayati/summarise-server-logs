6a895d26a026f71dd8e13ad9dce54dd455197748
Vahid Hedayati <vahid.hedayati@gmail.com>
2013-02-17 08:16:10 -0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Vahid Hedayati Feb 2013 - nice little script to summarise server logs into one over all report
+# This is the list of the attached summary files, in your own script you would need to run 
+# through your server/instance list and for each server output its content to file 
+
+# use the list of summary files as per below - 
+files="summary1 summary2 summary3"
+
+# Random number to be used whilst processing files
+RAND="$$"
+
+#Last file name
+oldname="";
+#Counter
+i=0
+
+# go through the list of files
+for names in $(echo $files); do
+        #increment counter
+        ((i++));
+        # Store very first file as last file name and move on
+        if [ $i == 1 ]; then
+                oldname=$names
+                shift;
+        else
+            # in all other cases create a random file of last file name
+            oldname1=$names.$RAND
+
+            # Now compare last file name - new or current file and get any values missing from new file and add it as a 0
+            # entry on previous file - this is a hack to ensure the awk statement below picks up all instances
+            for entries in $(awk -v i=$i 'NR==FNR { _[$1]=$2 } NR!=FNR { if (_[$1] == "") { if  ($2 ~ /[0-9]/)   { nn=0; nn=(_[$1]+=$2);  print FNR"-"$1"%0"} else { } } else { } }' $oldname $names); do
+                line=$(echo ${entries%%-*})
+                content=$(echo ${entries#*-})
+                content=$(echo $content|tr "%" " ")
+                #Using ed edit old file and add in the entry with a 0 value 
+                edit=$(ed -s $oldname  << EOF
+$line
+a
+$content
+.
+w
+q
+EOF 
+)
+              #Carry out edit and echo out all the output to /dev/null
+              $edit  >/dev/null 2>&1
+
+            done
+
+            # Now process the previous file vs current file and add up any rows that has two columns and 2nd column is a numeric value
+                awk -v i=$i 'NR==FNR { _[$1]=$2 } NR!=FNR { if (_[$1] != "") { if  ($2 ~ /[0-9]/)   { nn=0; nn=($2+_[$1]); print $1" "nn; } else { print $1;} }else { print; } }' $names $oldname> $oldname1
+        oldname=$oldname1
+    fi
+done
+
+# echo out the final sum
+cat $oldname
+
+# delete all randomly created files
+rm summary?.*
